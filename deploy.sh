11a22d886b7f238c3f9d706c21be69878dd809e4
aaron <amag1@protonmail.com>
2019-07-26 19:49:26 -0400

    fucking added auto deploy system

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,19 @@
 #/bin/bash
 
-
-for x in $(ls config); do
-ln -sf $x $HOME/.config/$x
+for x in $(ls ./config); do
+   echo "copying config/$x to $HOME/.config/$x"
+   rm -R $HOME/.config/$x
+   cp -R ./config/$x $HOME/.config/$x
 done
 
+
+for x in $(ls -a ./hf); do
+   if [ "$x" != "." ] && [ "$x" != ".." ]; then
+   echo "copying hf/$x to $HOME/$x"
+      rm -R $HOME/$x
+      cp -R ./hf/$x $HOME/$x
+   fi
+done
+
+source $HOME/.bashrc
 
